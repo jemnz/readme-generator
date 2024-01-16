328a76eb9affd7a291f23127c529a24fa1a9db5e
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2024-01-16 23:35:39 +0000

    added content to generatemarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+![License](https://img.shields.io/badge/license-${data.license}-blue)
+
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+- Email: ${data.email}
 `;
 }
 
